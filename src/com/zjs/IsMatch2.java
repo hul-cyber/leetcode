50bdb74ee49e224039e252a9c38c982088bfd134
hul-cyber <2197316937@qq.com>
2020-10-10 09:09:04 +0800

    提交新的剑指Offer算法题:剑指Offer20、21

---
+++ src/com/zjs/IsMatch2.java
@@ -0,0 +1,15 @@
+package com.zjs;
+
+/**
+ * @ClassName IsMatch2
+ * @Description 动态规划解决字符串匹配
+ * @Author zhangjusheng
+ * @Date 2020/10/8 14:58
+ * @Version 1.0
+ **/
+public class IsMatch2 {
+    public static void main(String[] args) {
+        String s = "a";
+        s.length();
+    }
+}
---
+++ src/com/zjs/ReorderOddEven2.java
@@ -0,0 +1,44 @@
+package com.zjs;
+
+/**
+ * @ClassName ReorderOddEven2
+ * @Description 调整数组顺序使奇数位于偶数之前 题目链接: https://leetcode-cn.com/problems/diao-zheng-shu
+ * -zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof/
+ * 首位双指针解法,最坏情况下:时间复杂度:O(n) 空间复杂度O(1)
+ * @Author zhangjusheng
+ * @Date 2020/10/9 23:29
+ * @Version 1.0
+ **/
+public class ReorderOddEven2 {
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5};
+        exchange(nums);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+
+    public static int[] exchange(int[] nums) {
+        if (nums == null)
+            return null;
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            while (start < nums.length && (nums[start] & 1) == 1) {
+                //如果是奇数,就一直向后找
+                start++;
+            }
+            while (end > -1 && ((nums[end] & 1) == 0)) {
+                //如果是偶数,就一直向前找
+                end--;
+            }
+            //交换双方的位置
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+        return nums;
+    }
+}
