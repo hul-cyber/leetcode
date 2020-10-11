b7ec00d0f3b7b3fb0af4462247cddf0fd2267710
hul-cyber <2197316937@qq.com>
2020-10-11 10:45:59 +0800

    添加文件:剑指Offer26 树的子结构多种解法 IsSubStructure1.java IsSubStructure2.java,剑指Offer25 合并两个排序的链表多种解法 MergeSortedLists1.java MergeSortedList2.java,剑指Offer24 反转链表多种解法 ReverseList1.java ReverseList2.java

---
+++ src/com/zjs/IsSubStructure2.java
@@ -0,0 +1,41 @@
+package com.zjs;
+
+/**
+ * @ClassName IsSubStructure2
+ * @Description 剑指Offer26 树的子结构 题目链接:https://leetcode-cn.com/problems/shu-de-zi-jie-gou-lcof/
+ * 使用双递归(优化)求解数的子结构 最坏情况下: 时间复杂度O(MN) 空间复杂度O(M) M: 树A的节点数量 N: 树B的节点数量
+ * @Author zhangjusheng
+ * @Date 2020/10/11 9:56
+ * @Version 1.0
+ **/
+public class IsSubStructure2 {
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        if (A == null || B == null)
+            return false;
+        if (isSubStructureOfTree(A, B)) {
+            return true;
+        } else {
+            return isSubStructure(A.left, B) || isSubStructure(A.right, B);
+        }
+    }
+
+    public boolean isSubStructureOfTree(TreeNode A, TreeNode B) {
+        if (B == null)
+            return true;
+        else if (A == null)
+            return false;
+        else
+        if (A.val == B.val) {
+            return isSubStructureOfTree(A.left, B.left) && isSubStructureOfTree(A.right, B.right);
+        } else {
+            return false;
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
---
+++ src/com/zjs/ReverseList2.java
@@ -0,0 +1,33 @@
+package com.zjs;
+
+/**
+ * @ClassName ReverseList2
+ * @Description 剑指Offer 24 反转链表 题目链接: https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof/
+ * 通过迭代的方法 最坏情况下:时间复杂度:O(n) 空间复杂度O(1)
+ * @Author zhangjusheng
+ * @Date 2020/10/10 12:57
+ * @Version 1.0
+ **/
+public class ReverseList2 {
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode next = null;
+
+        while (cur != null) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        
+        return pre;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+}
